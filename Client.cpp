7b58191ec5ec260a83125a2c7d078e2280e7dc76
Vladislav <vladislav-bodnya@mail.ru>
2025-04-15 09:41:11 +0300

    Made the cycles in Server and Client apps

--- Client.cpp
+++ Client.cpp
@@ -5,13 +5,20 @@
 
 #ifdef _WIN32
     #include <winsock2.h>
-    typedef SOCKET SocketType;
+    #include <ws2tcpip.h>
     #pragma comment(lib, "ws2_32.lib")
+    typedef SOCKET SocketType;
+    #define CLOSESOCKET closesocket
+    #define GET_ERROR WSAGetLastError()
 #else
     #include <sys/socket.h>
     #include <arpa/inet.h>
     #include <unistd.h>
     typedef int SocketType;
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define CLOSESOCKET close
+    #define GET_ERROR errno
 #endif
 
 int main() {
@@ -26,53 +33,56 @@ int main() {
 
     // 1. Создаем сокет клиента
     SocketType clientSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSock < 0) {
+    if (clientSock == INVALID_SOCKET) {
         std::cerr << "Failed to create client socket.\n";
         return 1;
     }
 
     // 2. Настройка адреса сервера: 127.0.0.1 (локальный компьютер) и порт 54000
-    sockaddr_in serverAddr;
+    sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(54000);
 #ifdef _WIN32
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 #else
     if(inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
-        std::cerr << "Неверный IP адрес\n";
+        std::cerr << "Wrong IP adress\n";
         return 1;
     }
 #endif
 
     // 3. Подключаемся к серверу
-    if (connect(clientSock, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (connect(clientSock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         std::cerr << "Failed to connect to server\n";
+        CLOSESOCKET(clientSock);
+        #ifdef _WIN32
+                WSACleanup();
+        #endif
         return 1;
     }
     std::cout << "Client: connection is successful!\n";
 
-    // 4. Отправляем данные серверу
-    std::string message = "Hello, server!";
-    send(clientSock, message.c_str(), message.size(), 0);
-    std::cout << "Client: message was sent\n";
-
-    // 5. Ждем и принимаем подтверждение от сервера
+    std::string userInput;
     char buffer[1024];
-    memset(buffer, 0, sizeof(buffer));
-    int bytesReceived = recv(clientSock, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived > 0) {
-        std::cout << "Client: server answered: " << buffer << "\n";
-    } else {
-        std::cerr << "Failed to recieve answer from server\n";
+
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, userInput);
+        if (userInput == "exit") break;
+
+        send(clientSock, userInput.c_str(), userInput.size(), 0);
+
+        int bytesReceived = recv(clientSock, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = '\0';
+            std::cout << "Answer: " << buffer << "\n";
+        }
     }
 
-    // 6. Закрываем сокет
-#ifdef _WIN32
-    closesocket(clientSock);
-    WSACleanup();
-#else
-    close(clientSock);
-#endif
+    CLOSESOCKET(clientSock);
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
 
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -1,17 +1,43 @@
 // Server.cpp
 #include <iostream>
+#include <algorithm>
 #include <cstring>  // Для memset и strlen
 
 #ifdef _WIN32
     #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
     typedef SOCKET SocketType;
-    #pragma comment(lib, "ws2_32.lib")  // Обязательно при компиляции под Windows!
+    #define CLOSESOCKET closesocket
+    #define GET_ERROR WSAGetLastError()    
 #else
+    #include <sys/types.h>
     #include <sys/socket.h>
     #include <netinet/in.h>
+    #include <arpa/inet.h>
     #include <unistd.h>
+    #include <errno.h>
     typedef int SocketType;
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define CLOSESOCKET close
+    #define GET_ERROR errno
 #endif
+
+std::string processData(const std::string& input) {
+    std::string result = input;
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+    return result;
+}
+
+bool setIPAddress(sockaddr_in& addr, const std::string& ipStr) {
+    #ifdef _WIN32
+        addr.sin_addr.s_addr = inet_addr(ipStr.c_str());
+        return addr.sin_addr.s_addr != INADDR_NONE;
+    #else
+        return inet_pton(AF_INET, ipStr.c_str(), &addr.sin_addr) > 0;
+    #endif
+}
 
 int main() {
 #ifdef _WIN32
@@ -25,26 +51,32 @@ int main() {
 
     // 1. Создаем сокет
     SocketType serverSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSock < 0) {
+    if (serverSock == INVALID_SOCKET) {
         std::cerr << "Failed to create socket\n";
         return 1;
     }
 
     // 2. Настройка адреса: используем IPv4, порт 54000, любой IP-адрес на данном компьютере
-    sockaddr_in serverAddr;
+    sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;             // IPv4
     serverAddr.sin_port = htons(54000);           // Порт (htons преобразует число в сетевой порядок байт)
-    serverAddr.sin_addr.s_addr = INADDR_ANY;      // Принимаем подключения с любого IP
-
+    if (!setIPAddress(serverAddr, "127.0.0.1")) {
+        std::cerr << "Wrong IP adress\n";
+        CLOSESOCKET(serverSock);
+        return 1;
+    }
+    
     // 3. Привязываем сокет к адресу
-    if (bind(serverSock, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        std::cerr << "Failed to configure socket\n";
+    if (bind(serverSock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Failed bind(): " << GET_ERROR << "\n";
+        CLOSESOCKET(serverSock);
         return 1;
     }
 
     // 4. Начинаем слушать – говорим системе, что сокет готов принимать входящие подключения
-    if (listen(serverSock, 5) < 0) {
-        std::cerr << "Failed to listen socket\n";
+    if (listen(serverSock, 1) == SOCKET_ERROR) {
+        std::cerr << "Failed listen(): " << GET_ERROR << "\n";
+        CLOSESOCKET(serverSock);
         return 1;
     }
     std::cout << "Server is working, waiting for connection...\n";
@@ -57,34 +89,31 @@ int main() {
     socklen_t clientAddrSize = sizeof(clientAddr);
 #endif
     SocketType clientSock = accept(serverSock, (sockaddr*)&clientAddr, &clientAddrSize);
-    if (clientSock < 0) {
+    if (clientSock == INVALID_SOCKET) {
         std::cerr << "Failed to get connection\n";
         return 1;
     }
     std::cout << "Client connected.\n";
 
-    // 6. Принимаем данные от клиента
     char buffer[1024];
-    memset(buffer, 0, sizeof(buffer));  // Обнуляем буфер
-    int bytesReceived = recv(clientSock, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived <= 0) {
-        std::cerr << "Failed to get Data or client disconnected\n";
-    } else {
-        std::cout << "Message recieved: " << buffer << "\n";
-        // 7. Отправляем подтверждение (например, "Сообщение получено")
-        const char* ack = "Message recieved";
-        send(clientSock, ack, strlen(ack), 0);
+    int bytesReceived;
+
+    while ((bytesReceived = recv(clientSock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytesReceived] = '\0';
+        std::string received(buffer);
+
+        std::cout << "Recieved: " << received << "\n";
+
+        std::string response = processData(received);
+        send(clientSock, response.c_str(), response.size(), 0);
     }
 
-    // 8. Закрываем сокеты
-#ifdef _WIN32
-    closesocket(clientSock);
-    closesocket(serverSock);
-    WSACleanup();
-#else
-    close(clientSock);
-    close(serverSock);
-#endif
+
+    CLOSESOCKET(clientSock);
+    CLOSESOCKET(serverSock);
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
 
     return 0;
 }
